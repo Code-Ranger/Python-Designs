a436dded31374c0bad26417bb2c56111e853544f
Code-Ranger <96903927+Code-Ranger@users.noreply.github.com>
2022-01-19 07:16:41 -0800

    Add files via upload

---
+++ design.py
@@ -0,0 +1,11 @@
+# Hello Viewers In this video we will create a design using python
+
+from turtle import *
+speed(10)
+color('cyan')
+bgcolor('black')
+b = 200
+while b > 0:
+    left(b)
+    forward(b * 3)
+    b = b - 1
